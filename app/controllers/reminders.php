85b281656d4a44eddb95b33117a665888905afed
rusinha22 <rupalisinha231@gmail.com>
2025-07-04 09:22:00 +0000

    add reminder controller with CRUD methods

--- app/controllers/reminders.php
+++ app/controllers/reminders.php
@@ -1,15 +1,48 @@
 <?php
 
-class Reminders extends Controller {
-  
-    public function index() {
-      $reminder = $this->model('Reminder');
-      $list_of_reminders = $reminder->get_all_reminders();
+class reminders extends Controller {
 
-        $this->view('reminders/index', ['reminders' => $list_of_reminders]);
+    public function index() {
+        // TEMP: use hardcoded user ID for now
+        $R = $this->model('Reminder');
+        $userId = 1; // change to $_SESSION['userid'] later
+        $reminders = $R->get_all_reminders($userId);
+
+        $this->view('reminders/index', ['reminders' => $reminders]);
     }
+
     public function create() {
-        $reminder = $this->model('Reminder');
         $this->view('reminders/create');
+    }
+
+    public function store() {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $R = $this->model('Reminder');
+            $R->create_reminder(1, $_POST['subject']); // change 1 to $_SESSION['userid'] later
+            header('Location: /reminders');
+            die;
+        }
+    }
+
+    public function edit($id) {
+        $R = $this->model('Reminder');
+        $reminder = $R->get_reminder_by_id($id);
+        $this->view('reminders/edit', ['reminder' => $reminder]);
+    }
+
+    public function update($id) {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $R = $this->model('Reminder');
+            $R->update_reminder($id, $_POST['subject']);
+            header('Location: /reminders');
+            die;
+        }
+    }
+
+    public function delete($id) {
+        $R = $this->model('Reminder');
+        $R->delete_reminder($id);
+        header('Location: /reminders');
+        die;
     }
 }
