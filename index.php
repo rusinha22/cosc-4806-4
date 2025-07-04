35a9e62aa48a0642e734f9ad101b41770012c63b
rusinha22 <rupalisinha231@gmail.com>
2025-07-04 09:24:33 +0000

    TEMP; set session userid manually for testing reminders

--- index.php
+++ index.php
@@ -1,4 +1,13 @@
-<?php 
+<?php
+session_start();  // required
+
+// TEMP SESSION FIX FOR TESTING
+$_SESSION['auth'] = 1;
+$_SESSION['userid'] = 1;
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 require_once 'app/init.php';
 
